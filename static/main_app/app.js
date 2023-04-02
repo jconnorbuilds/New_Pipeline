e4af79975d5de3c3dc0ac52b4c098e16c9c520c7
Joe Connor <joseph.connor.music@gmail.com>
2023-04-02 15:19:54 +0900

    url fix

--- static/main_app/app.js
+++ static/main_app/app.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     var table = $('#job_table').DataTable({
         paging: false,
         ajax: {
-            url: 'http://139.162.118.33/main-app/pipeline-data' + currentYear + '/' + currentMonth + '/',
+            url: 'http://139.162.118.33/main-app/pipeline-data/' + currentYear + '/' + currentMonth + '/',
             // url: 'http://127.0.0.1:8000/main_app/pipeline-data/' + currentYear + '/' + currentMonth + '/',
             dataSrc: function(json) {
                 // Sets these variables on initial page load

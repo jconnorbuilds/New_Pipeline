e242c5ab14b301a257c8f17bde73d2605e7c4de4
Joe Connor <joseph.connor.music@gmail.com>
2023-05-17 11:50:12 +0900

    added nginx

--- New_Pipeline/settings.py
+++ New_Pipeline/settings.py
@@ -202,6 +202,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -12,7 +12,4 @@ then
     
 fi
 
-python manage.py flush --no-input
-python manage.py migrate
-
 exec "$@"

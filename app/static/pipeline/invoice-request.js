274de8c10c63a285aef9663eb81c6a793a55edfb
Joe Connor <joseph.connor.music@gmail.com>
2023-08-18 20:40:56 +0900

    Fix submit btn bug and make error msg more helpful

--- app/static/pipeline/invoice-request.js
+++ app/static/pipeline/invoice-request.js
@@ -9,6 +9,19 @@ const slugify = str =>
 let hintWithInvoices = "Simply choose the matching job from the dropdown menu. When you've finished, click the Submit button below.";
 let hintWithNoInvoices = "Files will appear here once you've added them."
 
+const dropzoneMessages = document.querySelector(".dropzone-messages");
+const dropzoneErrorMessages = document.querySelector(".dropzone-error-messages");
+const pageTitle = document.querySelector(".content-wrapper h3");
+const contentWrapper = document.querySelector(".content-wrapper")
+
+function clearDropzone() {
+    $("#invoice-select-area").hide();
+    if (myDropzone.getRejectedFiles().length === 0) {
+        $('#invoice-hint').text(hintWithNoInvoices);
+        $('.dz-message').show();
+    };
+};
+
 var myDropzone;
 Dropzone.options.invoiceUploadForm = {
     autoProcessQueue: false,
@@ -27,24 +40,16 @@ Dropzone.options.invoiceUploadForm = {
     init: function() {
         myDropzone = this;
         // First change the button to actually tell Dropzone to process the queue.
-        // this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
         $("#invoice-upload-btn").on("click", (e) => { 
-          // Make sure that the form isn't actually being sent.
             e.preventDefault();
             e.stopPropagation();
             var allSelected = checkifAllJobsSelected();
             var noDuplicates = checkDuplicatesValidation();
             var rejectedFiles = myDropzone.getRejectedFiles.length;
-            console.log(`allSelected: ${allSelected}`)
-            console.log(`noDuplicates: ${noDuplicates}`)
-            console.log(`rejectedFiles: ${rejectedFiles}`)
             if (allSelected && noDuplicates && !rejectedFiles) {
                 myDropzone.processQueue();
             } else {
-                // TODO: display helpful info on the screen instead of in the console
-                console.log(`allSelected: ${allSelected}`)
-                console.log(`noDuplicates: ${noDuplicates}`)
-                console.log(`rejectedFiles: ${rejectedFiles}`)
+                // TODO: display helpful info on the screen
             }
         });
 
@@ -67,35 +72,23 @@ Dropzone.options.invoiceUploadForm = {
         this.on("successmultiple", function(files, response) {
           // Gets triggered when the files have successfully been sent.
           // Redirect user or notify of success.
-          window.location.replace('../invoice-uploader/send-email/')
+            dropzoneMessages.textContent = "All looks good!";
+            dropzoneErrorMessages.classList.add('hidden');
+            window.location.replace('../invoice-uploader/send-email/')
 
         });
-        this.on("errormultiple", function(files, response,) {
+        this.on("errormultiple", function(files, response) {
           // Gets triggered when there was an error sending the files.
           // Maybe show form again, and notify user of error
-            console.log(response);
-            for (file of files) {
-                $(`#${slugify(file.name)}-form`).remove();
-            }
-            if (myDropzone.getQueuedFiles().length == 0) { 
-                $("#invoice-select-area").hide();
-                if (myDropzone.getRejectedFiles().length == 0) {
-                    $('#invoice-hint').text(hintWithNoInvoices);
-                    $('.dz-message').show();
-                }
-            }
+            dropzoneErrorMessages.textContent = response;
+            dropzoneErrorMessages.classList.remove('d-none')
+            if (myDropzone.getQueuedFiles().length === 0) clearDropzone();   
         });
+
         this.on("removedfile", function(file) {
-            console.log('File removed!')
-            validateFormsAndFiles()
-            if (myDropzone.getQueuedFiles().length == 0) { 
-                $('#invoice-hint').text(hintWithNoInvoices);
-                $("#invoice-select-area").hide();
-                if (myDropzone.getRejectedFiles().length == 0) {
-                        $('.dz-message').show();
-                }
-            }
             $(`#${slugify(file.name)}-form`).remove();
+            validateFormsAndFiles()
+            if (myDropzone.getQueuedFiles().length == 0) clearDropzone();
         });
     }
 }
@@ -104,43 +97,50 @@ Dropzone.options.invoiceUploadForm = {
 function validateFormsAndFiles() {
     /*
     To avoid having duplicates or bad files uploaded to the server
-    and having to deal with them, this function checks
-    to make sure all invoices are valid and accounted for with a 
-    dropdown menu selection, and that there are no duplicates.
+    and having to deal with them, check to make sure all invoices are valid and accounted for 
+    with a dropdown menu selection, and that there are no duplicates.
     It runs after every time a file is added, removed, or 
     a selection is made one of the dropdown menus.
     */
     let noDuplicates = checkDuplicatesValidation();
     let allSelected = checkifAllJobsSelected();
 
-    console.log(`Rejected Files: ${myDropzone.getRejectedFiles().length}`)
-    console.log(`Queued Files: ${myDropzone.getQueuedFiles().length}`)
+    console.log(`allSelected: ${allSelected}`)
     console.log(`noDuplicates: ${noDuplicates}`)
-    if (myDropzone.getRejectedFiles().length == 0 
-        && myDropzone.getQueuedFiles().length > 0
-        && allSelected
-        && noDuplicates) {
-        $("#invoice-upload-btn").removeClass('disabled')
-        return true
-    } else if (myDropzone.getRejectedFiles().length > 0
-    || (myDropzone.files.length == 0) 
-    || noDuplicates == false 
-    || allSelected == false) {
-        $("#invoice-upload-btn").addClass('disabled')
-        return false
-    }
+    console.log(`rejectedFiles length: ${myDropzone.getRejectedFiles().length}`)
+    console.log(`queuedFiles length: ${myDropzone.getQueuedFiles().length}`)
+
+    if (
+        myDropzone.getRejectedFiles().length === 0 && 
+        myDropzone.getQueuedFiles().length > 0 && 
+        allSelected && noDuplicates
+        ) {
+            $("#invoice-upload-btn").removeClass('disabled');
+            return true;
+
+    } else if (
+        myDropzone.getRejectedFiles().length > 0 || 
+        myDropzone.files.length === 0 || 
+        noDuplicates === false || 
+        allSelected === false
+        ) {
+            $("#invoice-upload-btn").addClass('disabled');
+            return false;
+        };
+    console.log("queued files: ", getQueuedFiles())
+    console.log("dropzone files: ", myDropzone.files)
 };
 
 function checkDuplicatesValidation() {
     let dupChecker = [];
-    let duplicates = []
+    let duplicates = [];
 
     $('.invoice-select').each(function() {
         let currentVal = $(this).val();
-        if (currentVal == "0") { 
+        if (currentVal === "0") { 
             return;
         } else if (dupChecker.includes(currentVal)) {
-            duplicates.push(currentVal)
+            duplicates.push(currentVal);
         } else {
             dupChecker.push(currentVal);
         }
@@ -148,7 +148,7 @@ function checkDuplicatesValidation() {
 
     $('.invoice-select').each(function() {
         let currentVal = $(this).val();
-        if (currentVal == "0") { 
+        if (currentVal === "0") { 
             $(this).removeClass('is-valid');
             $(this).removeClass('is-invalid');
         } else if (duplicates.includes(currentVal)) {
@@ -160,18 +160,16 @@ function checkDuplicatesValidation() {
         }
     });
 
-    console.log(`duplicates: ${duplicates}`)
     return duplicates.length === 0;
 };
 
 function checkifAllJobsSelected() {
     let allSelected = true;
     $('.invoice-select').each(function() {
-        console.log(`SELECTED VALUE: ${$(this).val()}`)
         if ($(this).val() === "0") {
             allSelected = false;
             return false; // Exit the loop early if a zero value is found
-      }
+        };
     })
     return allSelected;
 };
@@ -196,7 +194,7 @@ $(document).ready(function(){
     When a file is uploaded, a dropdown is generated with all 
     costs that are awaiting invoices that are available to that vendor.
     */
-    let myDropzone = new Dropzone("#invoice-upload-form");
+    myDropzone = new Dropzone("#invoice-upload-form");
 
     const costs = JSON.parse(window.costsJson);
     const jobs = window.jobsJson;
@@ -224,7 +222,8 @@ $(document).ready(function(){
             <div class="input-group mb-3 invoice-select-row" id="${slugify(file.name)}-form">
                 <label for='invoice-select-${formNum}' class="input-group-text"><b>${file.name}</b></label>
                 <select class='form-select invoice-select' id='invoice-select-${formNum}' required></select>
-            </div>`);
+            </div>`
+            );
         $("#invoice-select-area").append(invoiceSelect).show();
         $("#invoice-hint").text(hintWithInvoices);
         invoiceSelect.find('select').append("<option value=0>Select job</option>")
@@ -253,7 +252,7 @@ $(document).ready(function(){
     });
     // Use event delegation to handle dynamically-generated elements
     $("#invoice-select-area").on("change", ".invoice-select", function() {
-        validateFormsAndFiles()
+        validateFormsAndFiles();
     });
 });
 

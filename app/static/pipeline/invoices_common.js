b1b93681668274b0b8fd47f3054123892ea49bf5
Joe Connor <joseph.connor.music@gmail.com>
2023-11-22 10:21:41 +0900

    Adds helpful info to the invoice info modal

--- app/static/pipeline/invoices_common.js
+++ app/static/pipeline/invoices_common.js
@@ -171,8 +171,16 @@ const InvoiceTable = (() => {
 
 const PayPeriod = (() => {
   const form = document.querySelector('form#pay-period-form');
-  const modal = new bootstrap.Modal('#pay-period-modal');
+  const modalEl = document.querySelector('#pay-period-modal');
+  const modal = new bootstrap.Modal(modalEl);
   const setMenu = (row) => row.querySelector('ul.dropdown-menu > li');
+  const descriptionDiv = document.querySelector(
+    '#pay-period-modal .invoice-desc'
+  );
+  modalEl.addEventListener('hide.bs.modal', () => {
+    form.reset();
+    descriptionDiv.innerHTML = '';
+  });
 
   const submitForm = (e, table) => {
     e.preventDefault();
@@ -183,15 +191,28 @@ const PayPeriod = (() => {
       '#pay-period-form #id_pay_period'
     ).value;
     Invoices.request(costID, table, payPeriod);
-    PayPeriod.modal.hide();
+
+    modal.hide();
   };
 
   const launchModal = (data) => {
     const idField = document.querySelector(
       '#pay-period-modal #id_cost_id'
     );
+
+    const descriptors = [
+      `Vendor name: ${data.vendor_name}`,
+      `PO number: ${data.PO_number}`,
+      `Job name: ${data.job_name}`,
+    ];
+    descriptors.forEach((str) => {
+      const pEl = document.createElement('p');
+      pEl.textContent = str;
+      descriptionDiv.appendChild(pEl);
+    });
+
     idField.value = data.id;
-    PayPeriod.modal.show();
+    modal.show();
   };
 
   return {
